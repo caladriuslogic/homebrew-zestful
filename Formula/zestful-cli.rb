b27caf64e125ad1b558be60b76f8de5dabe2e258
John Morrow <jmorrow@amoeba.org>
2026-03-20 16:13:24 -0700

    Upgrade CLI to v3.0.0 (Rust universal binary)

--- Formula/zestful-cli.rb
+++ Formula/zestful-cli.rb
@@ -1,9 +1,9 @@
 class ZestfulCli < Formula
   desc "CLI for sending notifications to the Zestful macOS app"
   homepage "https://zestful.app"
-  url "https://github.com/caladriuslogic/zestful-internal/releases/download/v1.0.0/zestful"
-  sha256 "b62714f01dffdaccd8248e294b41c1c2156dccc80b6df91ff1796efacd914118"
-  version "1.0.0"
+  url "https://github.com/caladriuslogic/zestful/releases/download/v3.0.0/zestful-3.0.0-macos.tar.gz"
+  sha256 "adc9b2dffd8ee0aef02c7ca0a2ff5a586c656566aab8589d397f75884f9323b2"
+  version "3.0.0"
   license "MIT"
 
   def install
@@ -11,6 +11,6 @@ class ZestfulCli < Formula
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/zestful 2>&1", 1)
+    assert_match "Usage", shell_output("#{bin}/zestful --help")
   end
 end
