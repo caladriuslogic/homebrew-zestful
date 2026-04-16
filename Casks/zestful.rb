b3a4baef9d472be2efab5055c42ee81671bfa9f0
John Morrow <jmorrow@amoeba.org>
2026-04-16 16:56:08 -0700

    Fix DMG URL to use public zestful repo

--- Casks/zestful.rb
+++ Casks/zestful.rb
@@ -2,7 +2,7 @@ cask "zestful" do
   version "1.1.0"
   sha256 "fce238670585c5daa8e6138037775b9f7b93d95a26c1cdcc0bfa6e746214afa8"
 
-  url "https://github.com/zestfuldevelopment/zestful-apple/releases/download/macos-v#{version}/Zestful.dmg"
+  url "https://github.com/zestfuldevelopment/zestful/releases/download/macos-v#{version}/Zestful.dmg"
   name "Zestful"
   desc "AI agent notifications for macOS"
   homepage "https://zestful.dev"
