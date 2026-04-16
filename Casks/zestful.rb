4d0f928b602c9f5070712fd5d50968b6874476ab
John Morrow <jmorrow@amoeba.org>
2026-04-16 13:51:01 -0700

    zestful 1.1.0

--- Casks/zestful.rb
+++ Casks/zestful.rb
@@ -1,11 +1,11 @@
 cask "zestful" do
-  version "1.0.0"
-  sha256 "413151986a6c2566d72d5c241765e8e514c3e3af5361025005af2db1beba74c6"
+  version "1.1.0"
+  sha256 "fce238670585c5daa8e6138037775b9f7b93d95a26c1cdcc0bfa6e746214afa8"
 
-  url "https://github.com/caladriuslogic/zestful/releases/download/macos-v#{version}/Zestful.dmg"
+  url "https://github.com/zestfuldevelopment/zestful-apple/releases/download/macos-v#{version}/Zestful.dmg"
   name "Zestful"
   desc "AI agent notifications for macOS"
-  homepage "https://zestful.app"
+  homepage "https://zestful.dev"
 
   app "Zestful.app"
 
