0f9cf8d32f000052249955bf9330f39e0c4a7f88
John Morrow <jmorrow@amoeba.org>
2026-04-16 17:52:51 -0700

    Update sha256 for re-signed 1.1.0 DMG

--- Casks/zestful.rb
+++ Casks/zestful.rb
@@ -1,6 +1,6 @@
 cask "zestful" do
   version "1.1.0"
-  sha256 "fce238670585c5daa8e6138037775b9f7b93d95a26c1cdcc0bfa6e746214afa8"
+  sha256 "9b59204780a24172c1fa645b3122557f49a8f14b3f11ea0ffdc91967e5bef2c7"
 
   url "https://github.com/zestfuldevelopment/zestful/releases/download/macos-v#{version}/Zestful.dmg"
   name "Zestful"
